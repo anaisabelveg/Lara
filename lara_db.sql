811e0d6b2a258eb2b9037fe5eb39c525aa953a3e
anaisabelveg <anaisabelveg@gmail.com>
2020-10-05 17:23:52 +0200

    Add files via upload

---
+++ lara_db.sql
@@ -0,0 +1,56 @@
+-- MySQL dump 10.13  Distrib 5.7.17, for macos10.12 (x86_64)
+--
+-- Host: 127.0.0.1    Database: LaraDB
+-- ------------------------------------------------------
+-- Server version	5.6.40
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `Alumnos`
+--
+
+DROP TABLE IF EXISTS `Alumnos`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Alumnos` (
+  `idAlumnos` int(11) NOT NULL,
+  `NOMBRE` varchar(45) DEFAULT NULL,
+  `APELLIDO` varchar(45) DEFAULT NULL,
+  `FECHA_NACIMIENTO` date DEFAULT NULL,
+  `REPETIDOR` tinyint(4) DEFAULT NULL,
+  `NOTA` double DEFAULT NULL,
+  PRIMARY KEY (`idAlumnos`),
+  UNIQUE KEY `idAlumnos_UNIQUE` (`idAlumnos`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Alumnos`
+--
+
+LOCK TABLES `Alumnos` WRITE;
+/*!40000 ALTER TABLE `Alumnos` DISABLE KEYS */;
+INSERT INTO `Alumnos` VALUES (1,'Pepe','Lopez','1972-07-01',1,5.3),(2,'Maria','Sanchez','1984-03-19',0,7.2);
+/*!40000 ALTER TABLE `Alumnos` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2020-10-05 17:08:11
